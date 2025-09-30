694744cbbcf58b6515aa5a3e0d549d4267f509e5
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-09-30 12:16:21 +0700

    Константы и переменные

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "time"
+import "fmt"
+
+// Объявление нескольких констант
+const (
+	A = true
+	B = "Строка"
+	C = 3.141
+)
 
 func main() {
-	print("Hello, World!\n")
-	time.Sleep(3 * time.Second)
+	var age int = 40
+	age2 := 50
+	age2 += 1
+	fmt.Println(age, age2)
+	fmt.Printf("Type: %T\n", age)
+	fmt.Printf("Type: %T\n", age2)
+	const PI = 3.14
+	fmt.Println(PI)
+	fmt.Printf("%T", PI)
 }
