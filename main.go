857c57fd0da0f19ca728d6f33ff3f85983094430
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-10-01 14:16:32 +0700

    byte

--- main.go
+++ main.go
@@ -2,21 +2,7 @@ package main
 
 import "fmt"
 
-// Объявление нескольких констант
-const (
-	A = true
-	B = "Строка"
-	C = 3.141
-)
-
 func main() {
-	var age int = 40
-	age2 := 50
-	age2 += 1
-	fmt.Println(age, age2)
-	fmt.Printf("Type: %T\n", age)
-	fmt.Printf("Type: %T\n", age2)
-	const PI = 3.14
-	fmt.Println(PI)
-	fmt.Printf("%T", PI)
+	var b byte = 200
+	fmt.Println(b, string(b))
 }
