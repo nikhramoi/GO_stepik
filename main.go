a2d630683c4c25e27b8199bb3336e6371bde05bf
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-10-02 08:59:50 +0700

    iota

--- main.go
+++ main.go
@@ -2,7 +2,37 @@ package main
 
 import "fmt"
 
+/* const (
+	red = iota
+	green
+	blue
+) */
+
+/* const (
+	_ = iota // ignore first value by assigning to blank identifier
+	monday
+	tuesday
+	wednesday
+	thursday
+	friday
+	saturday
+	sunday
+)
+
 func main() {
-	var b byte = 200
-	fmt.Println(b, string(b))
+	fmt.Println(monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+} */
+
+const (
+	a = iota
+	b
+	c = 5
+	d
+	e = iota
+	f = iota - 2
+	g = iota - 2
+)
+
+func main() {
+	fmt.Println(a, b, c, d, e, f, g)
 }
