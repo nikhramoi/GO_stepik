8c6cfda6451ae37fe4c7d4af5dc7c0e2e51c233f
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-10-03 14:54:06 +0700

    Создание и импорт пакетов

--- main.go
+++ main.go
@@ -1,53 +1,13 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
+	//ранее выполнена go mod init main (main - название программы)
+	"main/nettest" //тут создан пакет в папке nettest она соответствует директиве в файлах package nettest, поскольку в
+	// go.mod указан module main то обращаемся к пакету "main/nettest"
+	//. "main/nettest" - можно так но в сложных проектах так лучше не делать, окращает обращение к импортируемым методам и функциям
 )
 
 func main() {
-	arr := [3]int{1, 2, 3}
-	//var arr2 := [...]{"qwe","sad","asdasd"}
-	var arr3 = [...]string{"qwe", "sad", "asdasd"}
-	arr5 := [...]string{"foo", "bar"}
-	arr3[0] = "qwe"
-	arr3[1] = "sad"
-	arr3[2] = "asdasdsadsadsadasdasd"
-	fmt.Println(arr, arr3, arr5)
-
-	var slice = make([]string, 5, 5)
-	fmt.Println(slice, len(slice), cap(slice))
-	slice[0] = "qwe"
-	slice[1] = "sad"
-	slice[2] = "asdasdsadsadsadasdasd"
-	slice[3] = "asdasdsadsadsadasdasd"
-	slice[4] = "asdasdsadsadsadasdasd"
-	fmt.Println(slice, len(slice), cap(slice))
-	slice = append(slice, "foo")
-	slice = append(slice, "bar")
-	fmt.Println(slice, len(slice), cap(slice))
-	slice = append(slice, "barbasisca")
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Карты map аналог словарей и питухона
-	map1 := map[string]int{
-		"Вася":        25,
-		"Пупкин Иван": 45,
-	}
-	map1["Петров Петр"] = 30
-	delete(map1, "Вася")
-	fmt.Println(map1["Михаил"])
-	map1["Михаил"]++
-	fmt.Println(map1)
-
-	//Структуры
-	type Point struct {
-		x int
-		y int
-	}
-	p := Point{
-		x: 5,
-		y: 19,
-	}
-	p.x = 25
-	fmt.Println(p, p.x, p.y)
+	nettest.Say_hello()
 }
---
+++ nettest/nettest.go
@@ -0,0 +1,12 @@
+package nettest
+
+import "fmt"
+
+func Say_hello() {
+	fmt.Println("Вызван nettest")
+	kakajatohujna()
+}
+
+func kakajatohujna() {
+	fmt.Println("Вызвали хуйню")
+}
