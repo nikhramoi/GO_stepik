c89bcdc2f3dfded331b495fb8844ca10a6f4d467
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-10-03 13:58:43 +0700

    map и struct

--- main.go
+++ main.go
@@ -27,4 +27,27 @@ func main() {
 	fmt.Println(slice, len(slice), cap(slice))
 	slice = append(slice, "barbasisca")
 	fmt.Println(slice, len(slice), cap(slice))
+
+	// Карты map аналог словарей и питухона
+	map1 := map[string]int{
+		"Вася":        25,
+		"Пупкин Иван": 45,
+	}
+	map1["Петров Петр"] = 30
+	delete(map1, "Вася")
+	fmt.Println(map1["Михаил"])
+	map1["Михаил"]++
+	fmt.Println(map1)
+
+	//Структуры
+	type Point struct {
+		x int
+		y int
+	}
+	p := Point{
+		x: 5,
+		y: 19,
+	}
+	p.x = 25
+	fmt.Println(p, p.x, p.y)
 }
